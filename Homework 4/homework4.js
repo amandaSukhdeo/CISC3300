80d69c8fdfd40eb7e144eeb93431809da70d44cb
Amanda Sukhdeo <157178976+amandaSukhdeo@users.noreply.github.com>
2024-02-18 22:00:32 -0500

    Homework 4

---
+++ Homework 4/homework4.js
@@ -0,0 +1,74 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+]
+
+// appends all cats with adoptionStatus: 'available' to result[]
+function chooseCat(cats, result) {
+    for (let i = 0; i < cats.length; i++) {
+        if (cats[i].adoptionStatus == 'available') { 
+            result.push(cats[i].name); 
+        }
+    }
+}
+
+const result = []; 
+chooseCat(cats, result); // call chooseCat() function 
+
+// sentence about newly adopted cats 
+const sentence = `Today I adopted ${result.join(', ')}! Welcome home!`; 
+console.log(sentence); 
